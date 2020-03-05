6e93c37eace2ef060d2fb1de164faaae73cce28b
Roman Tymchenko <romatimchenko1984@gmail.com>
2020-03-05 16:17:44 +0200

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
+module.exports = function check(str,bracketsConfig) {
+
+  for (let i = 0; i < bracketsConfig.length;) {
+    let tempBracket = bracketsConfig[i].join('')
+    if (str.includes(tempBracket)) {
+      str = str.replace(tempBracket,'')
+      i = 0
+    } else {
+      i++
+    }
+  }
+  return str === '' ? true : false
+
+
 }
+
+
